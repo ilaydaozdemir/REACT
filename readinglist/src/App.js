e7442bebecc5e4627bb0bb81f34d001fa67a7ce5
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-09 20:57:49 +0300

    Reading List App (part 3)

--- readinglist/src/App.js
+++ readinglist/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import NewBookForm from './components.js/BookForm';
 import Booklist from './components.js/BookList';
 import Navbar from './components.js/Navbar';
 import BookContextProvider from './contexts/BookContext';
@@ -9,6 +10,7 @@ function App() {
       <BookContextProvider>
         <Navbar />
         <Booklist />
+        <NewBookForm />
       </BookContextProvider>
     </div>
   );
